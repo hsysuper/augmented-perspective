29cbc5e4599bd587a4c5ecc27873d1a441fea9f7
Jack He <hsysuper@gmail.com>
2022-02-07 21:26:19 -0800

    Create dataset.py

---
+++ dataset.py
@@ -0,0 +1,13 @@
+class Dataset:
+   def __init__(self, name)
+       pass
+      
+   
+   def get_number_of_images(self):
+       return 0
+       
+   def get_image(self):
+       '''
+       Return image1, image2, T
+       '''
+       pass
