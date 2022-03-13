ba4baa8a67ed9648ec17d5e7e30ea975bd4b055d
Jack He <hsysuper@gmail.com>
2022-03-13 00:27:27 -0800

    Fixes image input path handling in boosting model
    * Can process individual image files
    * Fixed a few image path issue
    * Save depth map as .npy format for augmented perspective

--- depth_model.py
+++ depth_model.py
@@ -1,6 +1,7 @@
 import argparse
 import contextlib
 import os
+import pathlib
 import numpy as np
 
 from models.monodepth2 import get_depth_map as get_monodepth_depth_map
@@ -30,34 +31,43 @@ From the augmented-perspective directory...
         python -m depth_model --boosting
 """
 
+
 class DepthModel:
-    def __init__(self, name: str, args):
+    def __init__(self, name: str, args, parser):
         self.name = name
-        vars(args)['image_path'] = '../../' + args.image_path
-        vars(args)['output_path'] = '../../outputs'
+        if os.path.isdir(args.image_path):
+            vars(args)['image_path'] = '../../' + args.image_path
+            vars(args)['image_files'] = None
+        else:
+            path = pathlib.Path(args.image_path)
+            vars(args)['image_path'] = '../../' + str(path.parent)
+            vars(args)['image_files'] = [str(path.name)]
+        vars(args)['output_path'] = '../../' + args.output_path
         self.args = args
+        self.parser = parser
 
 
 class MonodepthDM(DepthModel):
-    def __init__(self, name: str, args):
+    def __init__(self, name: str, args, parser):
         vars(args)['model_name'] = "mono_1024x320"
-        super().__init__(name, args)
+        super().__init__(name, args, parser)
 
     def get_depth_map(self):
-        return get_monodepth_depth_map(self.args)
+        print(self.args)
+        return get_monodepth_depth_map(self.args, self.parser)
 
 
 class BoostingDM(DepthModel):
-    def __init__(self, name: str, args):
+    def __init__(self, name: str, args, parser):
         vars(args)['net_receptive_field_size'] = 384
         vars(args)['patch_netsize'] = 2 * 384
         vars(args)['pix2pixsize'] = 1024
         vars(args)['max_res'] = np.inf
         vars(args)['gpu_ids'] = "-1"
-        super().__init__(name, args)
+        super().__init__(name, args, parser)
 
     def get_depth_map(self):
-        return get_boosting_depth_map(self.args)
+        return get_boosting_depth_map(self.args, self.parser)
 
 """
 Context manager to easily set working directory when running
@@ -74,13 +84,14 @@ def change_path(newdir):
 """
 Arg parser for CMD line.
 """
-def parse_args():
+def get_parser():
     parser = argparse.ArgumentParser(description='Depth model initialization.')
     parser.add_argument('--image_path', type=str, default='assets/',
                         help='path to a test image or folder of images')
     parser.add_argument('--monodepth2', action='store_true', default=False)
     parser.add_argument('--boosting', action='store_true', default=False)
-    return parser.parse_args()
+    parser.add_argument('--output_path', type=str, default='outputs/')
+    return parser
 
 
 DEPTH_MODELS = [
@@ -90,18 +101,26 @@ DEPTH_MODELS = [
 ]
 
 
-if __name__ == '__main__':
-    args = parse_args()
+def main():
+    parser = get_parser()
+    args = parser.parse_args()
+    os.makedirs(args.output_path, exist_ok=True)
 
-    # Run all of the models by default if none are specified.
+    # Run all the models by default if none are specified.
     if not args.monodepth2 and not args.boosting:
         vars(args)['monodepth2'] = True
         vars(args)['boosting'] = True
 
     for cls, name in DEPTH_MODELS:
-        if not vars(args)[name]: continue
+        if not vars(args)[name]:
+            continue
 
         args_cp = argparse.Namespace(**vars(args))
-        dm = cls(name, args_cp)
+        parser_cp = argparse.ArgumentParser(parents=[parser], add_help=False)
+        dm = cls(name, args_cp, parser_cp)
         with change_path(f'./models/{name}'):
             dm.get_depth_map()
+
+
+if __name__ == '__main__':
+    main()
