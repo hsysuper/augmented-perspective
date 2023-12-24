955b8997f4bbd2dbbb23bbdaedc431760867b628
Jack He <hsysuper@gmail.com>
2023-12-23 18:04:37 -0800

    Test Formatting change

--- augmented_perspective.py
+++ augmented_perspective.py
@@ -227,9 +227,7 @@ def run_augmented_perspective(
     logging.info(f"Start re-projection for angles: {angles}")
     durations = []
     for i in range(len(angles)):
-        logging.info(
-            f"Start re-projection at {angles[i]} degree clockwise around b axis"
-        )
+        logging.info(f"Start re-projection at {angles[i]} degree clockwise around b axis")
         start_time = time.time()
 
         # ROTATIONS
