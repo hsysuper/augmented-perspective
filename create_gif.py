8a6edb38b357df1e708debb06a2a1fffc7f2cedf
Jack He <hsysuper@gmail.com>
2023-12-23 23:46:04 -0800

    Improve scripts to create GIF using multiple perspectives
    * Add the ability to skip image generation to use existing image
    * Re-use argument parser from augmented_perspective module

--- create_gif.py
+++ create_gif.py
@@ -1,47 +1,48 @@
+import argparse
 import pathlib
 import sys
 
 from PIL import Image
+import reverse_argparse
 
-from augmented_perspective import run_augmented_perspective, parse_args
+from augmented_perspective import run_augmented_perspective, get_argument_parser
 """
 Usage:
-    python -m create_gif --image_path <path-to-image> --depth_model <model-name>
+    python -m create_gif --image_path <path-to-image> --depth_map_path <depth-map> \
+            --depth_model <model-name> --output_path <output_path>
 """
 
 if __name__ == '__main__':
-    # TODO: Manually adjust these 3 parameters before running the script.
-    ANGLE = 20
-    TRANSLATION = -0.5
-    FRAMES = 30
+    parser = get_argument_parser()
+    parser.add_argument(
+        "--use_existing_image",
+        type=bool,
+        help="Try to use existing image from output_path to create GIF",
+        action=argparse.BooleanOptionalAction,
+        default=False)
+    parser.set_defaults(angle=20, translation=-0.5, frames=30)
+    args = parser.parse_args(sys.argv)
 
-    args = parse_args()
-
-    image_name = pathlib.Path(args.image_path).stem
-    output_name = f"{image_name}_{args.depth_model}"
-    dirname = f"{output_name}_{ANGLE},{TRANSLATION}"
-    folder = pathlib.Path(f"output_images/{dirname}")
-
-    # Generate all frames of images.
-    run_augmented_perspective(sys.argv,
-                              save_filled_only=True,
-                              angle=ANGLE,
-                              translation=TRANSLATION,
-                              frames=FRAMES,
-                              output_directory=folder)
+    if not args.use_existing_image:
+        # Get another instance of the default parser so that the unparser can produce the correct command line arguments
+        default_parser = get_argument_parser()
+        unparser = reverse_argparse.ReverseArgumentParser(default_parser, args)
+        argv = unparser.get_effective_command_line_invocation().split()
+        # Generate all frames of images.
+        run_augmented_perspective(argv)
 
     # Create gif once all images have been generated.
-    files = list(pathlib.Path(folder).glob('*.png'))
+    files = list(pathlib.Path(args.output_path).glob('*.png'))
     files = list(map(str, files))
 
-    frame_idxs = [
+    frame_indices = [
         int(file[file.rfind('_') + 1:file.rfind('.')]) for file in files
     ]
-    files = [x for _, x in sorted(zip(frame_idxs, files))]
+    files = [x for _, x in sorted(zip(frame_indices, files))]
 
     images = [Image.open(p) for p in files]
 
-    images[0].save(folder / f"{dirname}.gif",
+    images[0].save(args.output_path / f"{args.output_path.stem}.gif",
                    save_all=True,
                    append_images=images[1:],
                    duration=100,
